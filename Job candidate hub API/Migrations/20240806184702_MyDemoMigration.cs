d1a226841fce429e0d1462bb2c1ebacc7a02445b
AyahFreij-Acabes <aya.freij@acabesinternational.com>
2024-08-06 21:51:45 +0300

    Adding migrations

---
+++ Job candidate hub API/Migrations/20240806184702_MyDemoMigration.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Job_candidate_hub_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class MyDemoMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Candidates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TimeInterval = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LinkedInProfile = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    GitHubProfile = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FreeTextComment = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Candidates", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Candidates");
+        }
+    }
+}
